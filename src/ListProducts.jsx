1a1dafa9d9fd9c9b473fbad783907a4baaca12d9
Raylan Cordeiro de Souza <raylan.cordeiro@DIGITALCOLLEGE.LOCAL>
2024-03-08 20:52:22 -0300

    Implements getProducts

--- src/ListProducts.jsx
+++ src/ListProducts.jsx
@@ -1,38 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Linha from './Linha';
 
 const ListProducts = () => {
+
+let [products, setProducts] = useState([]);
+
+useEffect(() => {
+    fetch('http://localhost:3000/products')
+    .then(response => response.json())
+    .then(data => {
+        setProducts(data);
+        console.log(data);
+    })
+
+},[]);
+
+
   return (
     <>
 
-<div class="relative overflow-x-auto">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+<div className="relative overflow-x-auto">
+    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" className="px-6 py-3">
                     Nome do produto
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" className="px-6 py-3">
                     Preço
                 </th>
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-            </tr>
+            {
+                products.map(product => {
+                    return(
+                        <Linha
+                            name={product.name}
+                            price={product.price}
+                        />
+                    )
+                })
+            }
+
         </tbody>
     </table>
 </div>
