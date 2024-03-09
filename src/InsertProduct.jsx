952e43bb0c12525902778808854b8dba1b81f42c
Raylan Cordeiro de Souza <raylan.cordeiro@DIGITALCOLLEGE.LOCAL>
2024-03-08 21:55:09 -0300

    Implements POST

--- src/InsertProduct.jsx
+++ src/InsertProduct.jsx
@@ -1,21 +1,44 @@
 import React from 'react'
 
 const InsertProduct = () => {
+
+  function onSubmitHandle(event) {
+    event.preventDefault()
+    const name = event.target.elements.name.value
+    const price = event.target.elements.price.value
+
+    fetch('http://localhost:3000/products', {
+      method: 'POST',
+      body: JSON.stringify({
+        id: 5,
+        name: name,
+        price: price
+      })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
   return (
    <>
-   
-   
-
-<form className="max-w-sm mx-auto">
+<form
+  className="max-w-sm mx-auto"
+  onSubmit={(e)=> onSubmitHandle(e)}
+  >
   <div className="mb-5">
-    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do produto</label>
-    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Guitarra ibanez" required />
+    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do produto</label>
+    <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Guitarra ibanez" required />
   </div>
   <div className="mb-5">
-    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preço</label>
-    <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='R$:1000' required />
+    <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Preço</label>
+    <input type="number" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='R$:1000' required />
   </div>
-  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar</button>
+  <button
+    type="submit"
+    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    Cadastrar
+    </button>
 </form>
 
    </>
--- src/ListProducts.jsx
+++ src/ListProducts.jsx
@@ -10,15 +10,12 @@ useEffect(() => {
     .then(response => response.json())
     .then(data => {
         setProducts(data);
-        console.log(data);
     })
-
 },[]);
 
 
   return (
     <>
-
 <div className="relative overflow-x-auto">
     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
